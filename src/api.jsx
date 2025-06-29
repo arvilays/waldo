e1dfa782c1f2a637342887ee312fd63510a6b8ea
arvilays <123064316+arvilays@users.noreply.github.com>
2025-06-28 22:21:08 -0700

    Changed API_BASE_URL

--- src/api.jsx
+++ src/api.jsx
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = "https://waldo-api-ivmo.onrender.com";
 
 const apiClient = {
   async request(endpoint, { method = "GET", data = null } = {}) {
